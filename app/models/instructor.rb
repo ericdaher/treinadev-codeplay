73f678007e582ab639843d92e9964f9e34eebb38
ericdaher <ericdaher2@gmail.com>
2021-05-17 20:02:27 -0300

    Refactor model validations

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,6 @@
 class Instructor < ApplicationRecord
   has_one_attached :profile_picture
-  
-  validates_presence_of :name, :email, message: 'não pode ficar em branco'
-  validates_uniqueness_of :email, message: 'já está em uso'
+
+  validates :name, :email, presence: { message: 'não pode ficar em branco' }
+  validates :email, uniqueness: { message: 'já está em uso' }
 end
