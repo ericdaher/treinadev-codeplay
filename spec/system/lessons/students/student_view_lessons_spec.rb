02f905d61e01cc561495abd7bb43c2555062d89a
ericdaher <ericdaher2@gmail.com>
2021-06-01 00:14:27 -0300

    Add student view lesson test

--- spec/system/lessons/students/student_view_lessons_spec.rb
+++ spec/system/lessons/students/student_view_lessons_spec.rb
@@ -74,4 +74,24 @@ describe 'Student view lessons' do
     expect(current_path).to eq(course_path(course))
     expect(page).to have_link 'Comprar'
   end
+
+  it 'directly' do
+    user = User.create!(email: 'estudante@codeplay.com', password: '12345678')
+    capaldi = Instructor.create!(name: 'Peter Capaldi', email: 'peter@capaldi.com',
+                                 bio: 'Twelfth Doctor')
+
+    course = Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
+                   code: 'RUBYBASIC', price: 10, instructor: capaldi,
+                   enrollment_deadline: 1.month.from_now)
+
+    lesson = Lesson.create!(name: 'Classes e Objetos', duration: 20, description: 'Orientação a Objetos em Ruby', course: course)
+    Enrollment.create!(user: user, course: course, price: course.price)
+
+    login_as user, scope: :user
+    visit course_lesson_path(course, lesson)
+
+    expect(page).to have_content('Classes e Objetos')
+    expect(page).to have_content('Orientação a Objetos em Ruby')
+    expect(page).to have_content('20 minutos')
+  end
 end
